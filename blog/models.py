e134811570328670007d238c0fb643a614309126
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-07-16 20:43:38 +0545

    added draft feature for written blog post and use indexing to show latest posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 class Post(models.Model):
 
-   
+    #Enum
+    class Status (models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
 
 
     title = models.CharField(max_length=250)
@@ -14,7 +16,17 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now = True)
 
+    status = models.CharField(max_length = 2,
+                              choices = Status.choices, 
+                              default = Status.DRAFT)
 
+    # latest post channxa ko lagi use bhako orderiing = publish
+    #ani indexex le chai page number ni deko ani latest post lauda sabai check garnu bhanda index bta choose garnu sajilo hunxa 
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields = ['-publish']),
+        ]
 
     def __str__(self):
         return self.title
