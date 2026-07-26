92c23366de5f65412a836e8d1fc60f84164f1600
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-07-26 13:16:45 +0545

    used count function to count posts of a user

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,8 @@ from django.conf import settings
 from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
+from django.db.models import Count
+
  
 
 # class PostListView(ListView):
@@ -68,11 +70,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter (active = True)
     #Form for users to comment
     form = CommentForm()
+
+    #List of similar posts
+    post_tags_id = post.tags.values_list('id', flat = True)
+    similar_posts = Post.published.filter(tags__in = post_tags_id)\
+                        .exclude(id= post.id)
+    similar_posts = similar_posts.annotate(same_tags = Count('tags'))\
+                        .order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post':post,
                    'comments': comments,
-                   'form': form})
+                   'form': form,
+                   'similar posts': similar_posts})
 
 def post_share(request, post_id):
     #rertieve post by id
