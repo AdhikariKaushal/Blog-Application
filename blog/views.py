26246ca5e6a174a86c4820c3b79de0048d48c19f
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-07-29 11:48:19 +0545

    search view

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, SearchForm
 from comments.forms import CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
@@ -14,6 +14,8 @@ from django.views.decorators.http import require_POST
 
 from taggit.models import Tag
 from django.db.models import Count
+
+from django.contrib.postgres.search import SearchVector
 
  
 
@@ -127,6 +129,27 @@ def post_comment(request,post_id):
             'form':form,
             'comment': comment
         })
+
+
+#search filter using postgres
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search = SearchVector('title', 'body')
+                ).filter(search = query)
+
+    return render (request,
+                   'blog/post/search.html',
+                   {'form': form,
+                    'query': query,
+                    'results': results})
 
     
 
