49106513b9c7ecf40a1a1e07286e7ec0cb86c424
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-05 13:46:47 +0545

    created the forms for signup using usercreation forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -8,4 +11,10 @@ class EmailPostForm(forms.Form):
 
 class SearchForm(forms.Form):
     query = forms.CharField()
+
+#signUPForm
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email','password1', 'password2']
     
