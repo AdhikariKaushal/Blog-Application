48fc82ac458fe6a645bfa8792e8963353ae10774
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-06 11:40:36 +0545

    created the postform to post new forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Post
 
 
 class EmailPostForm(forms.Form):
@@ -17,4 +18,9 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email','password1', 'password2']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'status']
     
