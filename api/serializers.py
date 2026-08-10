81a4e00b13231581411855f138706ab85407dcd6
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-10 12:15:45 +0545

    added the slug and publish date to the read_only fields

--- api/serializers.py
+++ api/serializers.py
@@ -5,4 +5,4 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'slug', 'author', 'body', 'publish', 'status']
-        read_only_fields = ['author']
+        read_only_fields = ['author', 'slug', 'publish']
