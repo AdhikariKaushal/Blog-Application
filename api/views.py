f518eb11d45d03da8799509353cc5ddd9fd0a58d
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-10 11:37:43 +0545

    created the postlistcreateapiview for creating posts

--- api/views.py
+++ api/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-class PostListAPIView(generics.ListAPIView):
+class PostListCreateAPIView(generics.ListCreateAPIView):
     queryset = Post.published.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author = self.request.user)
 
 class PostDetailAPIView(generics.RetrieveAPIView):
     queryset = Post.published.all()
