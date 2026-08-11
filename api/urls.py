4e93638707efe263185bd5417946e1fe131002d3
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-11 14:04:23 +0545

    urls done for comments api

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('posts/', views.PostListCreateAPIView.as_view(), name='post_list_create_api'),
     path('posts/<int:pk>/', views.PostDetailAPIView.as_view(), name='post_detail_api'),
+    path('posts/<int:post_id>/comments/', views.CommentListCreateAPIView.as_view(), name='comment_list_create_api'),
 ]
