35eae0a8a350be59c59a45a4b38e2c64e451e86d
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-11 14:02:34 +0545

    created the commentserializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from blog.models import Post
 
+from comments.models import Comment
+
 class PostSerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(read_only = True)
 
@@ -9,3 +11,9 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'slug', 'author', 'body', 'publish', 'status']
         read_only_fields = ['author', 'slug', 'publish']
         validators = []
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'email', 'body', 'created', 'active']
+        read_only_fields = ['created', 'active']
