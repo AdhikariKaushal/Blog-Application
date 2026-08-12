6f11f943eebdf182c754a57e377303d6929ebdca
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-12 13:03:52 +0545

    added the urls of swagger

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 sitemaps = {
     'post': PostSitemap
@@ -29,4 +30,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name = 'django.contrib.sitemaps.views.sitemap'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include ('api.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
