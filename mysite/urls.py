f212e01ce2a7ec2fd36bba798de09a5540791a4a
Adhikarikaushal <kaushaladhikari006@gmail.com>
2026-08-16 01:09:27 +0545

    added the urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -36,4 +36,5 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
